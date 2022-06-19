1a39a52c85fcf06a860a2f7f9bea2e55a358bfae
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-06-19 18:35:16 -0300

    Initial Commit

---
+++ MeuPrimeiroProjeto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeuPrimeiroProjeto
+//
+//  Created by Cecilia Andrea Pesce on 19/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
